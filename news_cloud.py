ae9ef186cb12495426dbd487a3aa711694a4bb3e
zhenshan <zhenshan@belmont.tech>
2022-03-22 16:29:13 -0500

    add location extraction

--- news_cloud.py
+++ news_cloud.py
@@ -10,13 +10,14 @@ logging.getLogger().setLevel(logging.INFO)
 
 st.text_input("search query", key="query")
 
-# query = "apple"
+# query = "china"
 # call google news to get data
 gn_d = Daisi("GoogleNews JSON", base_url="https://dev3.daisi.io")
 wc_d = Daisi("WordCloud JSON", base_url="https://dev3.daisi.io")
+le_d = Daisi("LocationExtraction", base_url="https://dev3.daisi.io")
 if st.session_state.query:
     x = gn_d.get_news(query=st.session_state.query, num=50)
-    # x = gn_d.get_news(query=query)
+    # x = gn_d.get_news(query=query, num=3)
     news = pd.DataFrame(x.get_result()["result"])
 
     # display a list of news 
@@ -30,3 +31,10 @@ if st.session_state.query:
     wordcloud = y.get_result()
     dat = datetime.now().strftime('%m/%d/%Y')
     st.image(wordcloud[1]["src"], f"What is about {st.session_state.query} today ({dat})")
+    
+    location_texts = [r['title'] for i, r in news.iterrows()]
+    # for i, row in news.iterrows():
+    z = le_d.get_locations(texts=location_texts[:10])
+    rows = [{"lat": i["latitude"], "lon": i["longitude"]} for i in z.get_result()["result"]]
+    map_df = pd.DataFrame(rows)
+    st.map(map_df)
